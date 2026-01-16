a7f2fc397dd563d19f45fa93c5ab4dae0add23ce
Oleg Korneev <olvikorneev@gmail.com>
2026-01-16 22:02:38 +0300

    Change the name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="p-8">
-      <h1 className="text-3xl font-bold">Мой статический сайт</h1>
+      <h1 className="text-3xl font-bold">Мой статический сайт #1</h1>
     </main>
   );
 }
